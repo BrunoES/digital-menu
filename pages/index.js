d0b20dd537cea9723c5478c8932bee30c9930a05
Bruno Henrique <brunoh915@gmail.com>
2023-05-04 19:55:31 -0300

    Listagem de pedidos do usuário.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Card from '../src/components/card/card.js';
 import styles from "../styles/Home.module.css";
 import checkoutStyles from "../src/components/checkout/Checkout.module.css";
+import pedidosStyles from "../src/components/pedidos/Pedidos.module.css";
 import ReactModal from 'react-modal';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
@@ -15,10 +16,22 @@ ReactModal.defaultStyles.content.marginRight = '5px';
 ReactModal.defaultStyles.content.marginBottom = '20px';
 
 const App = () => {
-  const [modalIsOpened, setModalIsOpened] = useState(false);
+   // Variáveis de sessão
+   const [customerId, setCustomerId] = useState(0);
+
+   // Modals
+  const [modalCartIsOpened, setModalCartIsOpened] = useState(false);
+  const [modalMeusPedidosIsOpened, setModalMeusPedidosIsOpened] = useState(false);
+
+  // Cardápio
   const [menuItems, setMenuItems] = useState([]);
+
+  // Carrinho
   const [cart, setCart] = useState([]);
   const [totalCart, setTotalCart] = useState(0);
+  
+  // Consulta Meus Pedidos
+  const [meusPedidos, setMeusPedidos] = useState([]);
 
   const [openAlertRemoveItemCheckout, setOpenAlertRemoveItemCheckout] = useState(false);
   const [msgAlert, setMsgAlert] = useState("");
@@ -40,19 +53,39 @@ const App = () => {
                })
          });
 
+         setCustomerId(1);
          setMenuItems(tempMenuItems);
         })
         .catch((err) => {
            console.log(err.message);
         });
+        
   }, []);
 
-   function openModal() {
-      setModalIsOpened(true);
-  }
+   function loadMeusPedidos() {
+      fetch(`http://192.168.0.18:8080/pedidos/${customerId}`)
+      .then((response) => response.json())
+      .then((data) => {
+         console.log(data);
+       setMeusPedidos(data);
+      })
+   }
 
-  function closeModal() {
-      setModalIsOpened(false);
+   function openModalCart() {
+      setModalCartIsOpened(true);
+   }
+
+   function closeModalCart() {
+      setModalCartIsOpened(false);
+   }
+
+   function openModalMeusPedidos() {
+      loadMeusPedidos();
+      setModalMeusPedidosIsOpened(true);
+   }
+
+   function closeModalMeusPedidos() {
+      setModalMeusPedidosIsOpened(false);
    }
 
    function removeCartItemByIndex(index) {
@@ -77,7 +110,7 @@ const App = () => {
       })}
 
       var checkout = {
-         customerId: 1,
+         customerId: customerId,
          customerName: "Bruno Henrique",
          total: totalCart,
          obs: "Sem katchup",
@@ -98,6 +131,11 @@ const App = () => {
       setMsgAlert("");
    }
 
+   function formatDate(stringDate) {
+      var datePedido = new Date(stringDate);
+      return (datePedido.toLocaleDateString() + " às " + datePedido.getHours() + ":" + datePedido.getMinutes());
+   }
+
   return (
    <div>
       <Snackbar open={openAlertRemoveItemCheckout} autoHideDuration={1500} onClose={handleCloseAlert}>
@@ -107,8 +145,8 @@ const App = () => {
       </Snackbar>
       <div className={styles.container}>
             <ReactModal
-               isOpen={modalIsOpened}
-               onRequestClose={closeModal}
+               isOpen={modalCartIsOpened}
+               onRequestClose={closeModalCart}
                appElement={this}
                preventScroll={true}
                contentLabel='Finalizar Pedido'>
@@ -132,6 +170,31 @@ const App = () => {
                   <button className={checkoutStyles.buttonFinish} onClick={() => save()}>Finalizar</button>
                </div>
          </ReactModal>
+         <ReactModal
+               isOpen={modalMeusPedidosIsOpened}
+               onRequestClose={closeModalMeusPedidos}
+               appElement={this}
+               preventScroll={true}
+               contentLabel='Meus Pedidos'>
+               <div className={checkoutStyles.modal}>
+                  <div className={checkoutStyles.checkoutItems}>
+                     <table className={checkoutStyles.table}>
+                     {meusPedidos.map((pedido) => {
+                        var index = meusPedidos.findIndex(x => x.id === pedido.id);
+                        return (
+                           // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+                           <tr className={pedidosStyles.pedido}>
+                              <td className={checkoutStyles.checkoutSubTotal}>R$ {pedido.total}</td>
+                              <td className={pedidosStyles.dateHour}>{formatDate(pedido.date_hour)}</td>
+                              <td className={pedidosStyles.pedidoObs}>{pedido.obs}</td>
+                           </tr>
+                        );
+                     })}
+                     </table>
+                  </div>
+                  <button className={checkoutStyles.buttonFinish} onClick={() => save()}>Finalizar</button>
+               </div>
+         </ReactModal>
 
          {menuItems.map((menuItem) => {
             return (
@@ -145,7 +208,8 @@ const App = () => {
          <div className={styles.endList}></div>
          <div className={styles.footer}>
             <p>R$ {totalCart}</p>
-            <button className={checkoutStyles.buttonCart} onClick={() => openModal()}>Pedido</button>
+            <button className={checkoutStyles.buttonCart} onClick={() => openModalCart()}>Carrinho</button>
+            <button className={checkoutStyles.buttonCart} onClick={() => openModalMeusPedidos()}>Meus Pedidos</button>
          </div>
       </div>
    </div>
